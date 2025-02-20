dbf4ddeda2f87b23276efd93e0d2a3753395702e
StoryChara <mjarah@unal.edu.co>
2025-02-20 18:45:31 -0500

    Taller 2

--- Taller 2/circulo.py
+++ Taller 2/circulo.py
@@ -1,4 +1,5 @@
 import math
 
 def area_circulo(radio:float) -> float:
+    """funcion que retorna el area de un circulo dado su radio"""
     return round(math.pi * (radio**2), 2)
--- Taller 2/modulo.py
+++ Taller 2/modulo.py
@@ -1,4 +1,5 @@
 from circulo import area_circulo
 
 def area_sombreada(lado:float) -> float:
+    """funcion que retorna el area sombreada de un cuadrado con un circulo en su interior"""
     return round((lado**2) - area_circulo(lado/2), 2)
