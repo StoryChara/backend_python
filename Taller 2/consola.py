8446ba6856a65975e4d946005abc65072da7e791
StoryChara <mjarah@unal.edu.co>
2025-02-20 19:14:34 -0500

    Taller 2

--- Taller 2/consola.py
+++ Taller 2/consola.py
@@ -1,4 +1,7 @@
 from modulo import area_sombreada
 
-lado = int(input("Hola! Por favor ingresa el lado del cuadrado \n >> "))
-print(f"El resultado del area sombreada es: {'{:.2f}'.format(area_sombreada(lado))}")
+try:
+  lado = int(input("Hola! Por favor ingresa el lado del cuadrado \n >> "))
+  print(f"El resultado del area sombreada es: {'{:.2f}'.format(area_sombreada(lado))}")
+except:
+  print("Debes ingresar un entero")
