98692294bb3168faf58f6eea7d0111db20a31c8f
StoryChara <mjarah@unal.edu.co>
2025-02-20 19:12:13 -0500

    Taller 2

--- Taller 2/modulo.py
+++ Taller 2/modulo.py
@@ -1,5 +1,9 @@
 from circulo import area_circulo
 
+def area_cuadrado(lado:float) -> float:
+    """funcion que retorna el area de un cuadrado dado su lado"""
+    return lado**2
+
 def area_sombreada(lado:float) -> float:
     """funcion que retorna el area sombreada de un cuadrado con un circulo en su interior"""
-    return round((lado**2) - area_circulo(lado/2), 2)
+    return round(area_cuadrado(lado) - area_circulo(lado/2), 2)
